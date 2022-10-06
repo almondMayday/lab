60a4aea4ba61c02980812cdcfe9dc30d919a18eb
Стефания Романова <stefa.romanova@gmail.com>
2022-10-06 14:36:35 +0300

    Revert "добавила вычисление секунд в минуте"
    This reverts commit 5705645157f5d50ff7e1d5705dfd302d16cf4cef.

--- DateTime.pas
+++ DateTime.pas
@@ -2,7 +2,5 @@
   var y:=readinteger('ввод');
   if (y mod 4=0) and (y mod 100 <> 0) and (y mod 400 = 0) then 
     print ('високосный')
-  else ('не високосный');
-  var min:= readinteger ('введите минуты');
-  var sec:= min*60;
+  else ('не високосный')
 end.
