d6c1f9dbb3e53120ff53e60f907cf250188d7f78
Стефания Романова <stefa.romanova@gmail.com>
2022-10-06 14:56:49 +0300

    добавила проверку того какая дата ближе к новому году

--- DateTime.pas
+++ DateTime.pas
@@ -2,5 +2,20 @@
   var y:=readinteger('ввод');
   if (y mod 4=0) and (y mod 100 <> 0) and (y mod 400 = 0) then 
     print ('високосный')
-  else ('не високосный')
+  else 
+    print ('не високосный');
+  var (d1, m1) :=readinteger2 ('введите день и месяц первой даты');
+  var (d2, m2) :=readinteger2 ('введите день и месяц второй');
+  var (md, mm):=(0,0);
+  if (12-m1)>(12-m2) then 
+    print (d2,'.',m2)
+  else
+    if m1=m2 then begin
+      if d1>d2 then
+        print (d1,'.',m1)
+      else 
+        print (d2,'.',m2)
+      end
+    else 
+      print (d1,'.',m1);
 end.
