5705645157f5d50ff7e1d5705dfd302d16cf4cef
Стефания Романова <stefa.romanova@gmail.com>
2022-10-06 14:34:22 +0300

    добавила вычисление секунд в минуте

--- DateTime.pas
+++ DateTime.pas
@@ -2,5 +2,7 @@
   var y:=readinteger('ввод');
   if (y mod 4=0) and (y mod 100 <> 0) and (y mod 400 = 0) then 
     print ('високосный')
-  else ('не високосный')
+  else ('не високосный');
+  var min:= readinteger ('введите минуты');
+  var sec:= min*60;
 end.
